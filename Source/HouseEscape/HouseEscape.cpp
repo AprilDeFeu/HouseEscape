6ed8dbdf2230790900c5e648440c2305456c8635
AprilDeFeu <AprilDeFeu@protonmail.com>
2022-02-26 18:35:00 -0500

    Created Unreal project

---
+++ Source/HouseEscape/HouseEscape.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HouseEscape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HouseEscape, "HouseEscape" );
